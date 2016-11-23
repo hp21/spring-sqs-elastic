8cb22cf4d77ba6b139c741ee547d5c4173780863
Peter Horvath <peter.horvath1@ge.com>
2016-11-23 15:50:14 +0100

    Initial commit

---
+++ src/main/java/org/hp/SpringSqsElasticApplication.java
@@ -0,0 +1,12 @@
+package org.hp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqsElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSqsElasticApplication.class, args);
+	}
+}
